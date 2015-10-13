3d8246b29900755b0baa243950cce8089819e47d
Davide Boscaini <davide.boscaini@gmail.com>
2015-10-13 18:05:13 +0200

    Cleaned 'compute_ROC.m' removing unnecessary computations

--- compute_ROC.m
+++ compute_ROC.m
@@ -11,37 +11,29 @@ if isfield(idxs,'gt_sym')
     tmp_idxs_gt     = find(tmp_idxs==1);
     tmp_idxs_gt_sym = find(tmp_idxs==2);
     dists_neg       = errs_(setdiff(1:length(errs_),union(tmp_idxs_gt,tmp_idxs_gt_sym)));
-    %dists_pos    = [errs_(idxs_.gt);errs_(idxs_.gt_sym)];
-    %dists_neg    = errs_(setdiff(1:length(errs_),union(idxs_.gt,idxs_.gt_sym)));
+    % previous code:
+    % dists_pos    = [errs_(idxs_.gt);errs_(idxs_.gt_sym)];
+    % dists_neg    = errs_(setdiff(1:length(errs_),union(idxs_.gt,idxs_.gt_sym)));
 else
     idxs_.gt  = sub2ind([n,n],idxs.gt,idxs.gt); 
     dists_pos = errs_(idxs_.gt);
     dists_neg = errs_(setdiff(1:length(errs_),idxs_.gt));
 end
 
-%[eer,~,~,~,~,~,~,fp,fn] = calculate_rates(dists_pos,dists_neg);
+%
 [fp,fn] = compute_fp_and_fn(dists_pos,dists_neg);
+
+% previous code:
+% [eer,~,~,~,~,~,~,fp,fn] = calculate_rates(dists_pos,dists_neg);
 
 %
 x_ = fp;
 y_ = 1-fn;
 
 %
-%idxs = floor(linspace(1,length(x_),1001));
-%x = x_(idxs);
-%y = y_(idxs);
-
-%
 n_samples = 101;
-% disp('1');
 x = linspace(min(x_),max(x_),n_samples)';
-
 y = interp1q(x_,y_,x);
-
-% disp('2');
-% tic;
-% y = interparc(x./max(x),x_(1:100000:end),y_(1:100000:end),'spline');
-% toc;
 
 end
 
@@ -49,11 +41,7 @@ function [fp,fn] = compute_fp_and_fn(dp,dn)
 np = length(dp);
 nn = length(dn);
 
-[d0, idx] = sort([dp(:); dn(:)], 'ascend');
-
-d0    = [d0(1)-1; d0(:)];
-delta = [d0(2:end)-d0(1:end-1); 0];
-d     = d0 + delta;
+[~, idx] = sort([dp(:); dn(:)], 'ascend');
 
 l = [ones(np,1); -ones(nn,1)];
 p = [0; (l(idx)>0)];
